26350f31643cec503017f2623bdf8a3ea5e8edd6
Yongsin Limwilaikul <88420101+yongsin-oak@users.noreply.github.com>
2025-06-23 21:56:25 +0700

    sdaf

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Mainlayout from "../layout/Mainlayout";
-import Home from "../pages/home";
-import User from "../pages/user";
 import Employee from "../pages/employee";
-import Login from "../pages/login";
-import ProductStock from "../pages/product-stock";
 import NotFoundPage from "../pages/error/404";
 import ErrorPage from "../pages/error/500";
-import PrivateRoute from "./PrivateRoute";
-import ECommerce from "../pages/sell/ecommerce";
+import Example from "../pages/example";
+import Home from "../pages/home";
+import Login from "../pages/login";
+import ProductStock from "../pages/product-stock";
 import Deliver from "../pages/sell/Delivery";
 import DirectSell from "../pages/sell/DirectSell";
+import ECommerce from "../pages/sell/ecommerce";
 import POS from "../pages/sell/POS";
-import Example from "../pages/example";
+import User from "../pages/user";
+import PrivateRoute from "./PrivateRoute";
 
 const Routers = () => {
   const genR = (path: string, element: React.ReactElement) => {
